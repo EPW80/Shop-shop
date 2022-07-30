a552b571141c8c5dd244a760abec09108038b6d2
Erik Williams <epfnw1@gmail.com>
2022-07-30 16:15:40 -0700

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Shopshop', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://admin:admin@shopshop.rqstm.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
