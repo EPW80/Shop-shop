40626eab45c720b13c1b22874ec0907c6e94ec8b
EWP80 <erikpw009@gmail.com>
2023-06-28 18:12:15 -0700

    init commit of refactored code

--- README.md
+++ README.md
@@ -1,26 +1,34 @@
 # Shop shop
 
-Shop Shop is an e-commerce application that allows users to shop for various items, add these products to the shopping cart, manage the shopping cart by increasing and decreasing quantities, check out their purchases and see their order history. 
+Welcome to Shop Shop, a dynamic e-commerce application designed to streamline your online shopping experience. With Shop Shop, users can explore various products, add their desired items to a shopping cart, manage their cart quantities with ease, and finally check out with a seamless process. Additionally, users have the option to review their order history for a comprehensive shopping overview.
 
 ## Built with
 
-- MongoDB
+- MongoDB: A source-available cross-platform document-oriented database program. Classified as a NoSQL database program, MongoDB uses JSON-like documents with optional schemas.
 
-- node.js
+- Node.js: An open-source, back-end JavaScript runtime environment that executes JavaScript code outside a web browser.
 
-- Redux
+- Redux: A predictable state container designed to help you write JavaScript apps that behave consistently across different environments and are easy to test.
 
-## Deployed Application
+## Local Installation and Setup
+
+To run Shop Shop on your local machine, follow these steps:
+
+```
+Clone this repository
+Navigate to the root directory in your terminal
+Run npm install to install the necessary dependencies
+Start the server by running npm start
+Visit localhost:3000 in your browser to view the application
+```
+
+## Deployment
 
 [Heroku](https://powerful-sands-15763.herokuapp.com/)
 
 ## Demo
 
 ![](./client/public/images/demo.gif)
-
-## Screenshot
-
-![](./client/public/images/shop.png)
 
 ## Contributor
 
--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { idbPromise } from "../../utils/helpers"
+import { idbPromise } from "../../utils/helpers";
 import CartItem from "../CartItem";
 import Auth from "../../utils/auth";
 import { useStoreContext } from "../../utils/GlobalState";
@@ -11,9 +11,9 @@ const Cart = () => {
 
   useEffect(() => {
     async function getCart() {
-      const cart = await idbPromise('cart', 'get');
+      const cart = await idbPromise("cart", "get");
       dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
-    };
+    }
 
     if (!state.cart.length) {
       getCart();
@@ -26,7 +26,7 @@ const Cart = () => {
 
   function calculateTotal() {
     let sum = 0;
-    state.cart.forEach(item => {
+    state.cart.forEach((item) => {
       sum += item.price * item.purchaseQuantity;
     });
     return sum.toFixed(2);
@@ -35,34 +35,33 @@ const Cart = () => {
   if (!state.cartOpen) {
     return (
       <div className="cart-closed" onClick={toggleCart}>
-        <span
-          role="img"
-          aria-label="trash">🛒</span>
+        <span role="img" aria-label="trash">
+          🛒
+        </span>
       </div>
     );
   }
 
   return (
     <div className="cart">
-      <div className="close" onClick={toggleCart}>[close]</div>
+      <div className="close" onClick={toggleCart}>
+        [close]
+      </div>
       <h2>Shopping Cart</h2>
       {state.cart.length ? (
         <div>
-          {state.cart.map(item => (
+          {state.cart.map((item) => (
             <CartItem key={item._id} item={item} />
           ))}
 
           <div className="flex-row space-between">
             <strong>Total: ${calculateTotal()}</strong>
 
-            {
-              Auth.loggedIn() ?
-                <button>
-                  Checkout
-                </button>
-                :
-                <span>(log in to check out)</span>
-            }
+            {Auth.loggedIn() ? (
+              <button>Checkout</button>
+            ) : (
+              <span>(log in to check out)</span>
+            )}
           </div>
         </div>
       ) : (
